bbd8621ad70fbe78be347a6af887e8d76ce32930
Christoph Schroeder <call-schroedi@web.de>
2019-01-25 23:20:09 +0100

    Hud in level

--- UI/UiItem.gd
+++ UI/UiItem.gd
@@ -2,7 +2,7 @@ extends TextureButton
 
 const BaseItem = preload("res://Items/BaseItem.gd")
 
-onready var ItemInfo = get_node("/root/Hud/ItemInfo")
+onready var ItemInfo = get_node("/root/GameLevel/Hud/ItemInfo")
 var Item:BaseItem = BaseItem.new()
 
 func _on_Item_toggled(button_pressed):
