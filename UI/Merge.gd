5a01d7680d732615b47fb5bdecbad2c786116f88
Christoph Schroeder <call-schroedi@web.de>
2019-01-25 23:00:58 +0100

    Merge items ui

---
+++ UI/Merge.gd
@@ -0,0 +1,23 @@
+extends Control
+
+onready var ItemContainer = get_node("../Inventory/Panel/ItemContainer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_Merge_pressed():
+	var items = ItemContainer.get_children()
+	var selected = []
+	for i in items:
+		if i.IsSelected():
+			selected.append(i)
+	
+	# TODO collect stats
+	
+	# TODO create new item and add it
+	
+	# remove merged items
+	for i in selected:
+		i.queue_free()
--- UI/UiItem.gd
+++ UI/UiItem.gd
@@ -4,3 +4,7 @@ extends TextureButton
 
 func _on_Item_toggled(button_pressed):
 	$Highlight.visible = button_pressed
+
+
+func IsSelected()->bool:
+	return $Highlight.visible
