30fc569d4a3abe3d4766d7d734caddf02815bf32
Christoph Schroeder <call-schroedi@web.de>
2019-01-26 13:44:52 +0100

    Add items to inventory, global stats

--- Scenes/ItemCollect.gd
+++ Scenes/ItemCollect.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
-
+onready var Inventory = get_node("/root/GameLevel/Hud/Inventory")
 
 func _on_ItemCollect_body_entered(body):
 	if "item" in body:
-		# TODO add body.item to inventory
+		Inventory.AddItem(body.item)
 		body.queue_free()
--- Stats/Stats.gd
+++ Stats/Stats.gd
@@ -1,8 +1,13 @@
 extends Node
 
+var CurrentStats = {}
+
 var AlltStats = [
 preload("res://Stats/Available/StatDamage.gd"), 
 preload("res://Stats/Available/StatSpeed.gd")]
 
 func GetAllStats():
 	return AlltStats
+
+func _ready():
+	CurrentStats["speed"] = 2
