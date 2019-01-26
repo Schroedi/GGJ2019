564a8da408ebd7b1422677be78fe4644b8f26a24
Christoph Schroeder <call-schroedi@web.de>
2019-01-26 20:47:38 +0100

    Z-Index of drag preview, hover info

--- UI/UiItem.gd
+++ UI/UiItem.gd
@@ -6,7 +6,6 @@ onready var ItemInfo = get_node("/root/GameLevel/Hud/ItemInfo")
 onready var Item:BaseItem
 
 func _on_Item_toggled(button_pressed):
-	ItemInfo.ShowItem(Item)
 	$Highlight.visible = button_pressed
 
 
@@ -17,3 +16,11 @@ func IsSelected()->bool:
 func get_drag_data(position):
     set_drag_preview(self.duplicate())
     return self
+
+
+func _on_UiItem_mouse_entered():
+	ItemInfo.ShowItem(Item)
+
+
+func _on_UiItem_mouse_exited():
+	pass # Replace with function body.
