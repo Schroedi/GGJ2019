062434feb6e7269a07d2c17dbadc76d325d79995
Rico <kirstein.rico@gmail.com>
2019-01-27 11:48:19 +0100

    added not enough gold message

--- Items/BaseItem.gd
+++ Items/BaseItem.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const popup = preload("res://Scenes/pop_label.tscn")
+
 var ItemStats = []
 var ItemLvl:int = 0
 # higher tier items are more expensive to level
@@ -21,7 +23,11 @@ func LevelUp():
 	var baseCost = ItemTier * 10
 	var cost = pow(baseCost, ItemLvl)
 	if (GameState.Level.playerGold < cost):
-		print("out of gold")
+		var pl = popup.instance()
+		pl.global_position = Vector2(340,600)
+		pl.setColor(Color.red)
+		pl.setLabel("Not enough Gold")	
+		GameState.Level.add_child(pl)
 		return
 	GameState.Level.playerGold -= cost
 	for s in ItemStats:
